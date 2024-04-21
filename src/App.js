7dafcda22c6f969647819c7c4c92c0404af78296
dcts <thomas@dcts.dev>
2024-04-21 11:11:31 +0200

    add loading states + chart basic implementation

--- src/App.js
+++ src/App.js
@@ -2,49 +2,89 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 import { formatNumberWithApostrophes } from "./utils/utils";
-import { fetchTokenInfo, fetchTokenHolders, radixResourceAddress } from "./utils/fetchData";
+import { fetchTokenInfo, fetchTokenHolders, dexterAddress } from "./utils/fetchData";
+
+import { Chart } from "react-google-charts";
+
+const options = {
+  title: "My Daily Activities",
+};
 
 function getResource() {
   if (window) {
     const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get('resource');
+    const resourceFromUrl = urlParams.get('resource') || dexterAddress;
+    console.log("resourceFromUrl");
+    console.log(resourceFromUrl);
+    return resourceFromUrl;
   }
-  return radixResourceAddress;
+  return dexterAddress;
 }
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
   const [tokenHolders, setTokenHolders] = useState([]);
   const [tokenInfo, setTokenInfo] = useState({});
 
+  const data = [["Address", "Token Quantity"], ["1", 123], ["2", 13]];
+  // console.log("tokenHolders.addesses");
+  // console.log(tokenHolders.addesses);
+  // tokenHolders.addesses?.slice(0,12).forEach(obj => {
+  //   const dataRow = [obj.addresses.account_id, obj.addresses.balance];
+  //   console.log("dataRow");
+  //   console.log(dataRow);
+  // });
+  // console.log(data);
   useEffect(() => {
     async function loadData() {
+      setIsLoading(true);
       const resource = getResource();
-      const [tokenInfo, tokenHolders] = await Promise.all([
+      const [tokenInfoResult, tokenHoldersResult] = await Promise.all([
         fetchTokenInfo(resource),
         fetchTokenHolders(resource),
       ]);
-      setTokenHolders(tokenHolders);
-      setTokenInfo(tokenInfo);
-      console.log({tokenInfo, tokenHolders});
+      setTokenHolders(tokenHoldersResult);
+      setTokenInfo(tokenInfoResult);
+      setIsLoading(false);
+      console.log({ tokenInfo, tokenHolders });
     }
     loadData();
   }, [])
-  return (
-    <div>
-      <h1>TOKEN xxxYYYzzz?</h1>
-      Total Holders: {tokenHolders?.length}
+
+  function renderTokenInfo() {
+    return <> <h1>TOKEN xxxYYYzzz?</h1>
+      Total Holders: {tokenHolders.holders}
       <p>Coin: {tokenInfo.name}</p>
       <p>description: {tokenInfo.description}</p>
       <p>address: {tokenInfo.address}</p>
       <p>iconUrl: {tokenInfo.iconUrl}</p>
       <p>website: {tokenInfo.website}</p>
-      <img src={tokenInfo.iconUrl} alt="token icon" width={80}/>
-      {tokenHolders?.map(obj => {
-        return <div className="flex asd">
+      <img src={tokenInfo.iconUrl} alt="token icon" width={80} />
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={"100%"}
+        height={"400px"}
+      />
+      {tokenHolders.addesses?.slice(0, 100).map((obj, indx) => {
+        return <div className="flex asd" key={indx}>
           <div className="truncate">{obj.account_id}</div>
           <div>{formatNumberWithApostrophes(obj.balance.toFixed(2))}</div>
         </div>
       })}
+    </>
+  }
+
+  function renderLoadingState() {
+    return <>
+      <h1>Loading...</h1>
+    </>
+  }
+
+  return (
+    <div>
+      {isLoading ? renderLoadingState() : renderTokenInfo()}
     </div>
   );
 }
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const radixResourceAddress = "resource_rdx1tknxxxxxxxxxradxrdxxxxxxxxx009923554798xxxxxxxxxradxrd";
+export const dexterAddress = "resource_rdx1tkktjr0ew96se7wpsqxxvhp2vr67jc8anq04r5xkgxq3f0rg9pcj0c";
 
-export async function fetchTokenInfo(resource = radixResourceAddress) {
+export async function fetchTokenInfo(resource = dexterAddress) {
   const result = await axios.get(`https://api.ociswap.com/tokens/${resource}`);
   return {
     name: result.data.name,
@@ -14,7 +14,12 @@ export async function fetchTokenInfo(resource = radixResourceAddress) {
   }
 }
 
-export async function fetchTokenHolders(resource = radixResourceAddress) {
-  const result = await axios.get(`https://gettokenholderscors-rvn75v2dcq-uc.a.run.app/?resource=${resource}`);
-  return result.data.data.data;
+export async function fetchTokenHolders(resource = dexterAddress, limit = 1000) {
+  const result = await axios.get(`https://gettokenholders-rvn75v2dcq-uc.a.run.app/?resource=${resource}&limit=${limit}`);
+  return {
+    addresses: result.data.addresses,
+    holders: result.data.holders,
+  };
 }
+
+
