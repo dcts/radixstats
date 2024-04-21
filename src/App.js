1c42e514ff2f20cc90d05d848da6742d55279532
dcts <thomas@dcts.dev>
2024-04-21 03:39:02 +0200

    working linking query param

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
+import {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function getResource() {
+  if (window) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('resource');
+  }
+  return "";
+}
+
 function App() {
+  const [tokenHolders, setTokenHolders] = useState([]);
+
+  useEffect(() => {
+    function loadTokenHolders() {
+      
+      fetch(`https://gettokenholderscors-rvn75v2dcq-uc.a.run.app/?resource=${getResource()}`)
+        .then(res => res.json())
+        .then(data => {
+          setTokenHolders(data.data.data);
+          console.log(data.data.data);
+        });
+    }
+    loadTokenHolders();
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>TOKEN xYz?</h1>
+        Total Holders: {tokenHolders?.length}
+        {tokenHolders?.map(obj => {
+          return <div className="flex asd">
+            <div className="truncate">{obj.account_id}</div>
+            <div>{obj.balance}</div>
+          </div>
+        })}
       </header>
     </div>
   );
